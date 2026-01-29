4709c4419107a18ad35dda98ac2275af19bbf1fa
Srimanyuacharyah <srimanyuacharya@gmail.com>
2026-01-29 09:24:39 +0530

    attendence_tracker

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return render_template("index.html")
     
 if __name__ == "__main__":
-    app.run(debug=True,host=" http://127.0.0.1:5000",port=5000)
+    app.run(debug=True, host="127.0.0.1", port=5000)
